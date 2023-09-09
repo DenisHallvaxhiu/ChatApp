41ac12587aca985b210bf1d05e6fa84668dcac27
DenisHallvaxhiu <dhallvaxhiu999@gmail.com>
2023-09-09 17:07:59 -0400

    Small layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import Header from './components/Header/Header'
-import './App.css'
+import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu"
+import "./App.css";
 
 function App() {
-
   return (
-    <>  
-      <Header/>
-    </>
-  )
+    <div className="screen">
+      <div className="left">
+        <Menu/>
+      </div>
+      <div className="right">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
